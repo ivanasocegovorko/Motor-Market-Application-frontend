027754b97642d2c12989b9998ec65c95c184b935
Ivana Soče Govorko <ivanasocegovorko@gmail.com>
2024-04-10 15:14:37 +0200

    generate vehicle service

---
+++ src/app/services/vehicle.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Vehicle } from '../models/vehicle';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VehicleService {
+
+  baseURL: string = "https://localhost:7025/api/vehicle";
+
+  constructor(private http: HttpClient) { }
+
+  getAllVehicles(): Observable<Vehicle[]> {
+      return this.http.get<Vehicle[]>(this.baseURL);
+  }
+
+
+  getVehicle(vehicleId: string): Observable<Vehicle> {
+    return this.http.get<Vehicle>(this.baseURL + "/" + vehicleId);
+  }
+
+
+  createVehicle(newVehicle: Vehicle) {
+      return this.http.post(this.baseURL, newVehicle);
+  }
+
+  editVehicle(vehicleId: string, editedVehicle: Vehicle): Observable<Vehicle> {
+      return this.http.put<Vehicle>(this.baseURL + "/" + vehicleId, editedVehicle);
+  }
+
+  deleteVehicle(vehicleId: string): Observable<any> {
+    return this.http.delete<any>(this.baseURL + "/" + vehicleId)
+  }
+}
