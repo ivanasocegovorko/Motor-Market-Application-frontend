f9f787d99ab002acddbdfb2bc0bd9b6fe27f945f
Ivana Soče Govorko <ivanasocegovorko@gmail.com>
2024-04-17 18:04:30 +0200

    make vehicle-new component html and ts

--- src/app/components/vehicle-new/vehicle-new.component.ts
+++ src/app/components/vehicle-new/vehicle-new.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Vehicle } from '../../models/vehicle';
+import { UserService } from '../../services/user.service';
+import { VehicleService } from '../../services/vehicle.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../../models/user';
+
 
 @Component({
   selector: 'app-vehicle-new',
   templateUrl: './vehicle-new.component.html',
   styleUrl: './vehicle-new.component.css'
 })
-export class VehicleNewComponent {
+export class VehicleNewComponent implements OnInit{
+  currentUser: User = new User  ();
 
+  newVehicle: Vehicle = new Vehicle();
+
+  vehicleList: Vehicle[] = [];
+
+  constructor(private vehicleService: VehicleService,private userService: UserService, private actRoute: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  addVehicle() {
+    this.vehicleService.createVehicle(this.newVehicle).subscribe(() => {
+      window.alert("Posted New Vehicle Successfully");
+      this.router.navigate(['vehicle']);
+    }, (error: { status: number; }) => {
+      console.log('Error: ', error)
+      if (error.status === 401 || error.status === 403) {
+        this.router.navigate(['signin']);
+      }
+    });
+  }
 }
